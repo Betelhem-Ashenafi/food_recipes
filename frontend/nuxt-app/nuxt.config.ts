7c8dc73bbfc61875c379aa99e8f76b919b623642
Betelhem-Ashenafi <betelhemafrem@gmail.com>
2025-12-07 02:36:56 +0300

    Update frontend nuxt.config.ts

--- frontend/nuxt-app/nuxt.config.ts
+++ frontend/nuxt-app/nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo'],
+
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: 'http://localhost:8080/v1/graphql',
+        tokenStorage: 'localStorage',
+        authHeader: 'Authorization',
+        authType: 'Bearer',
+        // @ts-ignore
+        devtools: { enabled: false }
+      }
+    }
+  }
 })
